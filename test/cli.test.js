2d72261e42d1a19009c9212f52565202d28943d0
khai96_ <hvksmr1996@gmail.com>
2018-01-24 15:32:41 +0700

    Improve the test

--- test/cli.test.js
+++ test/cli.test.js
@@ -2,6 +2,7 @@
 const {resolve, join} = require('path')
 const {readFileSync, readdirSync, statSync} = require('fs')
 const {spawnSync} = require('child_process')
+const fsForce = require('fs-force')
 
 const setOfLines = string => new Set(
   String(string)
@@ -17,33 +18,43 @@ describe('The command when everything aside itself works', () => {
     readFileSync(resolve(__dirname, 'data/cli-expect.txt'), 'utf8')
   )
 
-  const received = setOfLines(
-    spawnSync('node', [
-      resolve(__dirname, '../index.js'),
-      '--pnpm=echo',
-      `--local=${outputDir}`,
-      resolve(__dirname, 'data/cli-input.yaml')
-    ]).stdout
-  )
+  const testSuccess = description => {
+    const received = setOfLines(
+      spawnSync('node', [
+        resolve(__dirname, '../index.js'),
+        '--pnpm=echo',
+        `--local=${outputDir}`,
+        resolve(__dirname, 'data/cli-input.yaml')
+      ]).stdout
+    )
 
-  it('should print correct messages', () => {
-    expect(expected).toEqual(received)
-  })
-
-  describe('should create a folder tree', () => {
-    describe('whose sub-items', () => {
-      const names = readdirSync(outputDir)
-      const paths = names.map(x => resolve(outputDir, x))
-
-      it('have correct names', () => {
-        expect(new Set(names)).toEqual(new Set(['local1', 'local2']))
+    describe(description, () => {
+      it('should print correct messages', () => {
+        expect(expected).toEqual(received)
       })
 
-      it('are also folders', () => {
-        paths.forEach(x =>
-          expect(statSync(x).isDirectory()).toBe(true)
-        )
+      describe('should create a folder tree', () => {
+        describe('whose sub-items', () => {
+          const names = readdirSync(outputDir)
+          const paths = names.map(x => resolve(outputDir, x))
+
+          it('have correct names', () => {
+            expect(new Set(names)).toEqual(new Set(['local1', 'local2']))
+          })
+
+          it('are also folders', () => {
+            paths.forEach(x =>
+              expect(statSync(x).isDirectory()).toBe(true)
+            )
+          })
+        })
       })
     })
-  })
+  }
+
+  fsForce.deleteSync(outputDir)
+  testSuccess('when there was no output dir in the first place')
+  fsForce.writeFileSync(outputDir, 'it is a file')
+  testSuccess('when there output path was a file instead of a directory')
+  testSuccess('when the directory is at right place')
 })
